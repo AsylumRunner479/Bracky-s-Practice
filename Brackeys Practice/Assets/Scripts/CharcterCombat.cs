0d2ae559512a2c922cd996fc42048a113b54644b
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-12-14 13:00:31 +1100

    initial commit

---
+++ Brackeys Practice/Assets/Scripts/CharcterCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterStats))]
+public class CharcterCombat : MonoBehaviour
+{
+    public float attackSpeed = 1f;
+    private float attackCooldown = 0f;
+
+    CharacterStats myStats;
+    public void Attack (CharacterStats targetStats)
+    {
+        targetStats.TakeDamage(myStats.damage.GetValue());
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        myStats = GetComponent<CharacterStats>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Brackeys Practice/Assets/Scripts/Interact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : Interact
+{
+    PlayerHandler player;
+    CharacterStats myStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = PlayerHandler.instance;
+    }
+
+    // Update is called once per frame
+    public override void Use()
+    {
+        base.Use()
+        CharacterCombat playerCombat =  player.player.GetComponent<CharcterCombat>();
+        if (playerCombat != null)
+        {
+            playerCombat.Attack(myStats);
+        }
+    }
+}
